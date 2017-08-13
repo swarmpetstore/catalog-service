17d606f8dad7ffd4843b12390ae30e0ca77dcfb1
Tomasz Adamski <tadamski@redhat.com>
2017-08-14 01:15:55 +0200

    Initial commit

---
+++ src/main/java/org/packt/swarm/petstore/Pet.java
@@ -0,0 +1,32 @@
+package org.packt.swarm.petstore;
+
+public class Pet {
+
+    private int id;
+    private String name;
+    private int quantity;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+}
