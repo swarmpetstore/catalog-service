81c471ea9329d29fe5aed7b51a5ad227633d0014
Tomasz Adamski <tadamski@redhat.com>
2017-10-08 21:43:28 +0200

    Trace loadbalancing

---
+++ src/main/java/org/packt/swarm/petstore/PodNameResponseFilter.java
@@ -0,0 +1,16 @@
+package org.packt.swarm.petstore;
+
+import javax.ws.rs.container.ContainerResponseFilter;
+import javax.ws.rs.container.ContainerRequestContext;
+import javax.ws.rs.container.ContainerResponseContext;
+import javax.ws.rs.ext.Provider;
+import java.io.IOException;
+
+@Provider
+public class PodNameResponseFilter implements ContainerResponseFilter {
+    public void filter(ContainerRequestContext req, ContainerResponseContext res)
+            throws IOException
+    {
+        res.getHeaders().add("pod-name",System.getenv("HOSTNAME"));
+    }
+}
